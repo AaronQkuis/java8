cf471d664b7eff2d210b83c14e3fa3f5b7fa8174
Aaron <qikunlele@outlook.com>
2024-01-25 22:49:08 +0800

    Feat: init

---
+++ in-action/src/main/test/ActionTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * @author qikun
+ * @date 1/25/2024  10:35 PM
+ */
+public class ActionTest {
+    @Test
+    public void helloWorld() {
+        System.out.printf("hello world");
+    }
+}
