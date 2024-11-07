09b046ad4c33ec1e9d06a22fffb4d0e115fef0bb
Aaron <qikunlele@outlook.com>
2024-11-07 20:10:11 +0800

    Fix: one catch block catch different exceptions

---
+++ in-action/src/main/java/com/aaron/interview/io/IODemo.java
@@ -0,0 +1,15 @@
+package com.aaron.interview.io;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+/**
+ * @author qikun
+ * @date 11/7/2024  5:25 PM
+ */
+public class IODemo {
+    public static void main(String[] args) throws ArithmeticException, FileNotFoundException {
+        File file = new File("");
+
+    }
+}
--- in-action/src/main/test/com/aaron/interview/ActionTest.java
+++ in-action/src/main/test/com/aaron/interview/ActionTest.java
@@ -2,6 +2,11 @@ package com.aaron.interview;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+
 /**
  * @author qikun
  * @date 1/25/2024  10:35 PM
@@ -17,6 +22,29 @@ public class ActionTest {
     public void arrayNullTest() {
         int[] intArray = new int[3];
         System.out.println(intArray[0]);
+    }
+    @Test
+    public void arrayNullStringTest () throws FileNotFoundException {
+        File file = new File("");
+        String[] stringArray = new String[3];
+        System.out.println(stringArray[0].length());
+    }
+
+    /**
+     * 一个 catch 捕获多个异常
+     */
+    @Test
+    public void multiExceptionTest() {
+        try {
+            if (true) {
+                throw new IOException("Error");
+            } else {
+                throw new SQLException("Error");
+            }
+        } catch (IOException | SQLException e) {
+            System.out.println("Caught exception: " + e);
+            //e.getSQLState(); // 编译错误：Exception 类型中不存在此方法
+        }
     }
 
 
